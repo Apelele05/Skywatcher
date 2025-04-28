940cb8ee2c8516384114547ed1407b94558d579b
Apelele <kufaapelele@gmail.com>
2025-04-29 00:10:23 +0200

    Added axios

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = response.data.temperature.current;
+}
+
+function searchCity(city) {
+  let apiKey = "fo68et34d0e7d6af2ae13cd3f76c33b0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios
+    .get(apiUrl)
+    .then(refreshWeather)
+    .catch(function (error) {
+      console.error("Error fetching weather:", error);
+    });
+}
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
