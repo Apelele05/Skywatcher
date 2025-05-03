d29a6c1b70a5ce68a00a1f3adf44321d79ab7270
Apelele <kufaapelele@gmail.com>
2025-05-03 17:01:04 +0200

    Added the search functionality using the API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = response.data.temperature.current;
+  temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 
 function searchCity(city) {
   let apiKey = "fo68et34d0e7d6af2ae13cd3f76c33b0";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/current?query=Lisbon&key=fo68et34d0e7d6af2ae13cd3f76c33b0&units=metric";
+
   axios
     .get(apiUrl)
     .then(refreshWeather)
@@ -16,10 +22,10 @@ function searchCity(city) {
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
   searchCity(searchInput.value);
 }
-
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Miami");
